5773d5e0b1d01c340262fea5c1ba702bd6b08c53
Victor Piccoli <victorpicc@gmail.com>
2019-03-20 20:03:26 -0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <h1>Hello world</h1>
+);
+
+const wrapper = document.getElementById('root');
+
+ReactDOM.render(<App />, wrapper);
